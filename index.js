9465e0027ebbbaf06b2b00671c83fabf9b48efed
AyimWilliam <odhisayim@gmail.com>
2023-03-12 11:38:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    alert('Hee hee, that tickles!');
+
+button.addEventListener('click', addingEventListener);
 }
+
+document.getElementById('button').addEventListener('click', addingEventListener);
