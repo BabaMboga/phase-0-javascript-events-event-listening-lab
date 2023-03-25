c8f3e07f3248f62b71a711382fdc84de981a689d
AyimWilliam <odhisayim@gmail.com>
2023-03-25 15:02:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const input = document.getElementById('button');
 
 function addingEventListener() {
-    const input = document.getElementById('button');
+    
 
     alert('Hee hee, that tickles!');
 
 button.addEventListener('click', addingEventListener);
 }
 
-document.getElementById('button').addEventListener('click', addingEventListener);
+input.addEventListener('click', addingEventListener);
